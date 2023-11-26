14650dc383bae5495320258bbd34c26d8950e143
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-11-26 16:07:56 +0530

    Add CSS styles and update JavaScript file

--- client/src/components/MeetingPage.jsx
+++ client/src/components/MeetingPage.jsx
@@ -1,152 +1,127 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import socketIO from 'socket.io-client';
-import { Button, Grid, Typography } from "@mui/material"
+import { Button, Grid, Typography } from "@mui/material";
 import { CentralizedCard } from "./CentralizedCard";
 import { Video } from "./Video";
 
- let pc = new RTCPeerConnection({
-   iceServers: [
-     {
-       urls: "stun:stun.l.google.com:19302",
-     },
-   ],
- });
+let pc = new RTCPeerConnection({
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302",
+    },
+  ],
+});
 
- 
 export function MeetingPage() {
-    const [socket, setSocket] = useState(null);
-    const [meetingJoined, setMeetingJoined] = useState(false);
-    const [videoStream, setVideoStream] = useState();
-    const [remoteVideoStream, setRemoteVideoStream] = useState();
-    
-    const params = useParams();
-    const roomId = params.roomId;
+  const [socket, setSocket] = useState(null);
+  const [meetingJoined, setMeetingJoined] = useState(false);
+  const [videoStream, setVideoStream] = useState();
+  const [remoteVideoStream, setRemoteVideoStream] = useState();
+  
+  const params = useParams();
+  const roomId = params.roomId;
 
-    useEffect(() => {
-      const s = socketIO.connect("http://localhost:3001");
-      s.on("connect", () => {
-        setSocket(s);
-        s.emit("join", {
-          roomId,
+  useEffect(() => {
+    const s = socketIO.connect("http://localhost:3001");
+    s.on("connect", () => {
+      setSocket(s);
+      s.emit("join", {
+        roomId,
+      });
+
+      window.navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+          audio: true,
+        })
+        .then(async (stream) => {
+          setVideoStream(stream);
+          stream.getTracks().forEach((track) => {
+            pc.addTrack(track, stream);
+          });
+
+          pc.ontrack = (event) => {
+            event.streams[0].getTracks().forEach((track) => {
+              if (track.kind === 'video') {
+                setRemoteVideoStream(new MediaStream([track]));
+              }
+            });
+          };
+        })
+        .catch((error) => {
+          console.error('Error accessing media devices:', error);
         });
 
-        window.navigator.mediaDevices
-          .getUserMedia({
-            video: true,
-            audio:true
-          })
-          .then(async (stream) => {
-            setVideoStream(stream);
-          //   const videoTracks = stream.getVideoTracks()[0];
-          // const audioTracks = stream.getAudioTracks()[0];
-
-          // pc.addTransceiver(videoTracks, { streams: [stream] });
-          // pc.addTransceiver(audioTracks, { streams: [stream] });
-          });
-
-        s.on("localDescription", async ({ description }) => {
-          // Receiving video -
-          console.log({ description });
-          pc.setRemoteDescription(description);
-          pc.ontrack = (e) => {
-            setRemoteVideoStream(new MediaStream([e.track]));
-          };
-
-          s.on("iceCandidate", ({ candidate }) => {
-            pc.addIceCandidate(candidate);
-          });
-
-          pc.onicecandidate = ({ candidate }) => {
-            s.emit("iceCandidateReply", { candidate });
-          };
+      s.on("localDescription", async ({ description }) => {
+        try {
+          await pc.setRemoteDescription(description);
           await pc.setLocalDescription(await pc.createAnswer());
           s.emit("remoteDescription", { description: pc.localDescription });
-        });
-          s.on("remoteDescription", async ({ description }) => {
-            // Receiving video -
-            console.log({ description });
-            pc.setRemoteDescription(description);
-            pc.ontrack = (e) => {
-              setRemoteVideoStream(new MediaStream([e.track]));
-            };
-
-            s.on("iceCandidate", ({ candidate }) => {
-              pc.addIceCandidate(candidate);
-            });
-
-            pc.onicecandidate = ({ candidate }) => {
-              s.emit("iceCandidateReply", { candidate });
-            };
-
-         
-            //s.emit("remoteDescription", { description: pc.localDescription });
-          });
+        } catch (err) {
+          console.error(err);
+        }
       });
-    }, []);
 
-    if (!videoStream) {
-        return <div>
-            Loading...
-        </div>
-    }
+      s.on("remoteDescription", async ({ description }) => {
+        try {
+          await pc.setRemoteDescription(description);
+        } catch (err) {
+          console.error(err);
+        }
+      });
+    });
+  }, []);
 
-    if (!meetingJoined) {
-        return <div style={{minHeight: "100vh",}}>
-            <CentralizedCard>
-                <div>
-                    <Typography textAlign={"center"} variant="h5">
-                        Hi welcome to meeting {roomId}.
-                    </Typography>
-                </div>
-                <br/><br/>
-                <div style={{display: "flex", justifyContent: "center"}}>
-                    <Button onClick={async () => {
-                        // sending pc
-                        pc.onicecandidate = ({candidate}) => {
-                            socket.emit("iceCandidate", {candidate});
-                        }
-                pc.addTrack(videoStream.getVideoTracks()[0])
+  if (!videoStream) {
+    return <div>Loading...</div>;
+  }
+
+  if (!meetingJoined) {
+    return (
+      <div style={{ minHeight: "100vh" }}>
+        <CentralizedCard>
+          <div>
+            <Typography textAlign={"center"} variant="h5">
+              Hi welcome to meeting {roomId}.
+            </Typography>
+          </div>
+          <br/><br/>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              onClick={async () => {
+                pc.onicecandidate = ({ candidate }) => {
+                  socket.emit("iceCandidate", { candidate });
+                };
+                
                 try {
                   await pc.setLocalDescription(await pc.createOffer());
-                  console.log({ aa: pc.localDescription });
                   socket.emit("localDescription", { description: pc.localDescription });
                 } catch (err) {
-                  console.log({ msg: err?.message });
                   console.error(err);
                 }
-                    
-            
-                
-                // try {
-                //   await pc.setRemoteDescription(await pc.createOffer());
-                //   console.log({ aa: pc.remoteDescription });
-                //   socket.emit("remoteDescription", { description: pc.remoteDescription });
-                // } catch (err) {
-                //   console.log({ msg: err?.message });
-                //   console.error(err);
-                // }
-                // socket.on("remoteDescription", async ({ description }) => {
-                //   await pc.setRemoteDescription(description);
-                // });
-                // socket.on("iceCandidateReply", ({ candidate }) => {
-                //   pc.addIceCandidate(candidate)
-                // });
-                        setMeetingJoined(true);
-                    }} disabled={!socket} variant="contained">
-                        Join meeting
-                    </Button>
-                </div>
-            </CentralizedCard>
-        </div>
-    }
-    console.log({remoteVideoStream,videoStream})
-    return <Grid container spacing={2} alignContent={"center"} justifyContent={"center"}>
-        <Grid item xs={12} md={6} lg={4}>
-            <Video stream={videoStream} mute={true}/>
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-            <Video stream={remoteVideoStream} />
-        </Grid>
+
+                setMeetingJoined(true);
+              }}
+              disabled={!socket}
+              variant="contained"
+            >
+              Join meeting
+            </Button>
+          </div>
+        </CentralizedCard>
+      </div>
+    );
+  }
+
+  return (
+    <Grid container spacing={2} alignContent={"center"} justifyContent={"center"}>
+      <Grid item xs={12} md={6} lg={4}>
+        <Video stream={videoStream} mute={true}/>
+      </Grid>
+      <Grid item xs={12} md={6} lg={4}>
+        <Video stream={remoteVideoStream} />
+      </Grid>
     </Grid>
+  );
 }
